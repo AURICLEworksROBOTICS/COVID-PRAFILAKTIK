b5906bb39ebba07ef7808c547c4bade31bc196ec
AURICLE <70269704+AURICLEworksROBOTICS@users.noreply.github.com>
2020-12-17 12:31:40 +0530

    Add files via upload

---
+++ SIMPLE CODE TO PRINT  COVID PRAFILAKTIK ..py
@@ -0,0 +1,6 @@
+# this is just a rough code for printing COVID PRAFILAKTIK on screen !
+
+print ("COVID PRAFILAKTIK")
+
+visit our website : https://sites.google.com/view/auricle-robotics/home
+    
